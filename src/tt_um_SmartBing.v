37e7b6990e945ea09949c8dc1e89158b522afff2
Raito676 <juanmanuelperezcozatl@gmail.com>
2025-03-05 16:30:18 -0600

    Update tt_um_SmartBing.v

--- src/tt_um_SmartBing.v
+++ src/tt_um_SmartBing.v
@@ -31,7 +31,7 @@ module tt_um_SmartBing (
   // All output pins must be assigned. If not used, assign to 0.
     assign uo_out[1:0] = 2'00;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out[6:0] = 6'000000;
-  assign uio_oe  = 0;
+    assign uio_oe [7:0]  = 8'00000000;
 
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, clk, rst_n, uo_out[0:1], uio_out[0:6]};
